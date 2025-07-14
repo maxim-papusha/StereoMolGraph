20b688d1b5fb6793aa38aeee324e8bfe7e0a88c0
Maxim Papusha <maxim.papusha@ltt.rwth-aachen.de>
2025-07-14 09:25:59 +0200

    doc

--- docs/conf.py
+++ docs/conf.py
@@ -18,57 +18,55 @@ sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent / "src"))
 link_from = str(Path(__file__).resolve().parent.parent / "examples")
 link_to = str(Path(__file__).resolve().parent / "examples")
 
-
 os.symlink(link_from, link_to, target_is_directory=True)
 
 
-project = 'StereoMolGraph'
-copyright = '2025, Maxim Papusha'
-author = 'Maxim Papusha'
+project = "StereoMolGraph"
+copyright = "2025, Maxim Papusha"
+author = "Maxim Papusha"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-nb_execution_mode: Literal['off', 'force', 'auto', 'cache', 'inline'] = "off"
+nb_execution_mode: Literal["off", "force", "auto", "cache", "inline"] = "off"
 nb_execution_show_tb = True  # Show traceback in the output
-nb_execution_raise_on_error = True  # Critical - makes exceptions fail the build
+nb_execution_raise_on_error = True # Critical - makes exceptions fail the build
+
 nb_execution_allow_errors = False  # Don't allow errors in the output
 nb_execution_timeout = 300  # Timeout in seconds
 
 extensions = [
     # Core Sphinx extensions first
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.doctest',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.todo',
-    
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.doctest",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.todo",
     # Type hints should come after autodoc
-    'sphinx_autodoc_typehints',
-    
+    "sphinx_autodoc_typehints",
     # MyST extensions
-    'myst_nb',
-    
+    "myst_nb",
     # Theme and UI extensions
-    'sphinx_rtd_theme',
-    'sphinx_copybutton',
+    "sphinx_rtd_theme",
+    "sphinx_copybutton",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 
-autodoc_member_order: Literal["alphabetical", "bysource", "groupwise"] = "groupwise"
-#autodoc_class_signature = "separated"
+autodoc_member_order: Literal["alphabetical", "bysource", "groupwise"] = (
+    "groupwise"
+)
+# autodoc_class_signature = "separated"
 # Force type hints to be links when possible
-typehints_defaults: Literal['comma', 'braces', 'braces-after'] = 'braces-after'
+typehints_defaults: Literal["comma", "braces", "braces-after"] = "braces-after"
 
 # (Optional) Make the links more readable
 typehints_document_rtype_none = False
--- src/stereomolgraph/rdmol2graph.py
+++ src/stereomolgraph/rdmol2graph.py
@@ -1,4 +1,5 @@
 # pyright: standard
+# typing with rdkit is not fully supported
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
