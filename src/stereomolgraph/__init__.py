d555f7dc7a203bdcc6b7496efb0a46422f149a1b
Maxim Papusha <maxim.papusha@ltt.rwth-aachen.de>
2025-07-28 18:49:29 +0200

    update_initpy

--- src/stereomolgraph/__init__.py
+++ src/stereomolgraph/__init__.py
@@ -4,17 +4,50 @@ CondensedReactionGraph and StereoCondensedReactionGraph"""
 
 from __future__ import annotations
 
-from importlib import import_module
-from importlib.metadata import version
+from typing import TYPE_CHECKING
 
-def __getattr__(name):
-    """Lazy import submodules on first access (Python 3.7+)"""
-    if name in {'sub1', 'sub2'}:
-        return import_module(f'.{name}', __name__)
-    if name == "__version__":
-        return version("stereomolgraph")
+if TYPE_CHECKING:
+    from stereomolgraph.graphs.crg import CondensedReactionGraph
+    from stereomolgraph.graphs.mg import AtomId, Bond, MolGraph
+    from stereomolgraph.graphs.scrg import StereoCondensedReactionGraph
+    from stereomolgraph.graphs.smg import StereoMolGraph
 
-    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
+def __getattr__(name: str):
+    match name:
+        case "AtomId":
+            from stereomolgraph.graphs.mg import AtomId
+
+            return AtomId
+        case "Bond":
+            from stereomolgraph.graphs.mg import Bond
+
+            return Bond
+        case "MolGraph":
+            from stereomolgraph.graphs.mg import MolGraph
+
+            return MolGraph
+        case "StereoMolGraph":
+            from stereomolgraph.graphs.smg import StereoMolGraph
+
+            return StereoMolGraph
+        case "CondensedReactionGraph":
+            from stereomolgraph.graphs.crg import CondensedReactionGraph
+
+            return CondensedReactionGraph
+        case "StereoCondensedReactionGraph":
+            from stereomolgraph.graphs.scrg import StereoCondensedReactionGraph
+
+            return StereoCondensedReactionGraph
+        case "__version__":
+            from importlib.metadata import version
+
+            return version("stereomolgraph")
+        
+        case name:
+            from importlib import import_module
+            import_module(f'.{name}', __name__)
+
         
 
 
