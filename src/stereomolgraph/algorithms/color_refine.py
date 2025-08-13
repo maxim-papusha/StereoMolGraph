c8a36c6c1377820938f9445977927e599076e714
Maxim Papusha <maxim.papusha@ltt.rwth-aachen.de>
2025-08-13 21:40:13 +0200

    wip

--- src/stereomolgraph/algorithms/color_refine.py
+++ src/stereomolgraph/algorithms/color_refine.py
@@ -13,8 +13,13 @@ if TYPE_CHECKING:
     from stereomolgraph.graphs.mg import (
         AtomId,
         MolGraph,
+        StereoMolGraph,
     )
+    from stereomolgraph.stereodescriptors import Stereo, AtomStereo
 
+    S = TypeVar("S", bound=Stereo)
+    AS = TypeVar("AS", bound=AtomStereo)
+    BS = TypeVar("BS", bound=BondStereo)
     N = TypeVar("N", bound=int)
 
 def numpy_int_tuple_hash(
@@ -55,7 +60,16 @@ def numpy_int_tuple_hash(
         output += 97531
         return output
 
-def numpy_int_set_hash(): ...
+def numpy_int_multiset_hash(
+    arr: np.ndarray[tuple[int, ...], np.dtype[np.int64]],
+    out: None|np.ndarray[tuple[Literal[1], ...], np.dtype[np.int64]] = None,
+) -> np.ndarray:
+    """
+    Hash function for a multiset (order-independent with duplicates) of integers.
+    Works by sorting the elements and then applying the tuple hashing function.
+    """
+    sorted_arr = np.sort(arr, axis=-1)
+    return numpy_int_tuple_hash(sorted_arr, out)
 
 def label_hash(
     mg: MolGraph,
@@ -66,8 +80,13 @@ def label_hash(
     :param mg: MolGraph object containing the atoms.
     :param atom_labels: Iterable of attribute names to use for hashing.
     """
-    atom_hash = {atom: hash(tuple(
-        mg.get_atom_attribute(atom, attr ) for attr in atom_labels))
+    if len(atom_labels) == 1:
+        atom_label = atom_labels[0]
+        atom_hash = {atom: hash(mg.get_atom_attribute(atom, atom_label))
+        for atom in mg.atoms}
+    else:
+        atom_hash = {atom: hash(frozenset(
+        (attr, mg.get_atom_attribute(atom, attr)) for attr in atom_labels))
         for atom in mg.atoms
         }
     return atom_hash
@@ -154,3 +173,55 @@ def color_refine_mg(
 
     return {a: int(h) for a, h in zip(mg.atoms, atom_hash)}
 
+def color_refine_smg(
+    smg: StereoMolGraph,
+    max_iter: None|int = None,
+    atom_labels: Iterable[str] = ("atom_type",),
+) -> dict[AtomId, int]:
+
+    atom_label_hash = label_hash(smg, atom_labels)
+    atom_hash = np.array(
+        [atom_label_hash[atom] for atom in mg.atoms], dtype=np.int64
+    )
+
+    arr_id = {atom: a_id for a_id, atom in enumerate(mg.atoms)}
+
+    grouped_atom_stereo: dict[type[S], list[tuple[int, ...]]] = defaultdict(list)
+    atoms_with_atom_stereo: set[int] = set()
+
+    s_perm_groups = []
+    s_atoms = []
+    s_nbr_atoms = []
+
+    s_nbrs_hash_i = []
+    s_nbrs_hash_perm_i = []
+
+    for atom, stereo in smg.get_atom_stereo.items():
+        if stereo.parity is not None:
+            atoms_with_atom_stereo.add(atom)
+            nbr_atoms = stereo.atoms if parity != -1 else stereo.inverted_atoms
+            grouped_atom_stereo[stereo.__class__.PERMUTATION_GROUP].append(nbr_atoms)
+
+    
+    atoms_with_atom_stereo = set(smg.atoms) - atoms_with_atom_stereo
+    # TODO: for atoms without atom_stereo
+
+    for perm_group, nbr_atoms_list in grouped_atom_stereo.items():
+        
+        s_perm_groups.append(np.array(perm_group, dtype=np.uint8))
+
+        s_atoms.append(np.fromiter((s.central_atom for s in stereo_list),
+                        dtype=np.uint16))
+
+        nbr_atoms = np.array([[arr_id[a] for a in 
+                        (s.atoms if s.parity != -1 else s._inverted_atoms)]  
+                     for s in stereo_list ], dtype=np.uint16)
+
+        s_nbr_atoms.append(nbr_atoms)
+
+        nbrs_hash = np.empty_like(nbr_atoms, dtype=np.int64)
+        s_nbrs_hash_i.append(nbrs_hash)
+
+        nbrs_hash_perm = np.empty( (*(nbrs_hash.shape), len(perm_group)),
+                                 dtype=np.int64)
+        s_nbrs_hash_perm_i.append(nbrs_hash_perm)
