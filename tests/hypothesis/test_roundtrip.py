41fee83db51137f801dc35402a6b04aec3c3c397
Maxim Papusha <maxim.papusha@ltt.rwth-aachen.de>
2025-08-19 19:39:23 +0200

    wip

--- tests/hypothesis/test_roundtrip.py
+++ tests/hypothesis/test_roundtrip.py
@@ -3,7 +3,7 @@ import shelve
 
 from hypothesis import assume, given
 import hypothesis.strategies as st
-from hypothesis.stateful import Bundle, RuleBasedStateMachine, initialize, rule, invariant
+from hypothesis.stateful import Bundle, RuleBasedStateMachine, initialize, rule, invariant, precondition
 
 from stereomolgraph.periodic_table import PERIODIC_TABLE
 from stereomolgraph import MolGraph
@@ -21,25 +21,20 @@ class GraphModifier(RuleBasedStateMachine):
     #    if self._visited_path.exists():
     #        self._visited_path.unlink()
 
-    atoms = Bundle("atoms")
-    bonds = Bundle("bonds")
 
-    @rule(target=atoms,
-        atom_id=st.integers(), 
-        atom_type=st.sampled_from(list(PERIODIC_TABLE.values()))
-    )
+    @rule(atom_id=st.integers(),
+          atom_type=st.sampled_from(list(PERIODIC_TABLE.values())))
     def add_atom(self, atom_id, atom_type):
         assume(atom_id not in self.g.atoms)
         self.g.add_atom(atom_id, atom_type)
-        return atom_id
 
-    @rule(target=bonds,
-        atom_id1 = atoms,
-          atom_id2 = atoms)
-    def add_bond(self, atom_id1, atom_id2):
+    @precondition(lambda self: len(self.g.atoms) != 0)
+    @rule(data=st.data())
+    def add_bond(self, data):
+        atom_id1 = data.draw(st.sampled_from(list(self.g.atoms)))
+        atom_id2 = data.draw(st.sampled_from(list(self.g.atoms)))
         assume(atom_id1 != atom_id2)
         self.g.add_bond(atom_id1, atom_id2)
-        return frozenset({atom_id1, atom_id2})
 
     @invariant()
     def do_nothing(self):
