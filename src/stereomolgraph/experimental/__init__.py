2189f610f23eaaf992e2e01a12ea4d0532496601
Maxim Papusha <maxim.papusha@ltt.rwth-aachen.de>
2026-07-13 13:34:50 +0200

    bugfix

--- src/stereomolgraph/experimental/__init__.py
+++ src/stereomolgraph/experimental/__init__.py
@@ -6,3 +6,15 @@ Experimental functionality for StereoMolGraph.
    This module contains **experimental** features. The interfaces may
    change without notice between versions. Use at your own risk.
 """
+
+from stereomolgraph.experimental._isomers import (
+    generate_fleeting_stereoisomers,
+    generate_stereoisomers,
+)
+from stereomolgraph.experimental._json import JSONHandler
+
+__all__ = [
+    "generate_fleeting_stereoisomers",
+    "generate_stereoisomers",
+    "JSONHandler",
+]
